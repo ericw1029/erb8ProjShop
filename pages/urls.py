66e704b95f3cd0229d006aa616c6470d1dd41ae4
ericw1029 <ericw29tech@gmail.com>
2026-01-10 01:05:00 +0800

    Add Category ,product

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name ='pages'
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('index',views.index,name='index'),
     path('about',views.about,name='about')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 from django.http import HttpResponse
 
 
 # Create your views here.
 # todo
 # !
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    #return HttpResponse("product_list")
+    return render(request,'pages/index.html',{'category': category,'categories': categories,'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,'product/detail.html',{'product': product,'cart_product_form': cart_product_form})
+
 
 def index(request):
     print(request,request.path)
