fe456a0dc3f5b6ebe285fe5faa0c74e492a5a57c
ericw1029 <ericw29tech@gmail.com>
2026-01-15 00:10:25 +0800

    Style Checkout page

--- orders/forms.py
+++ orders/forms.py
@@ -3,6 +3,13 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    address = forms.CharField(max_length=100)
+    postal_code = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address','postal_code', 'city']
--- orders/views.py
+++ orders/views.py
@@ -2,26 +2,58 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 
+@login_required
 def order_create(request):
+    current_user = User.objects.get(id=request.user.id)
     cart = Cart(request)
     if request.method == 'POST':
+        checkoutEmail = request.POST["email"]
+        checkoutFirstName = request.POST["first_name"]
+        checkoutLastName = request.POST["last_name"]
+        
+        print("POST", checkoutEmail, checkoutFirstName, checkoutLastName)
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
             for item in cart:
-                OrderItem.objects.create(order=order,
-                                        product=item['product'],
-                                        price=item['price'],
-                                        quantity=item['quantity'])
+                OrderItem.objects.create(order=order,product=item['product'],
+                                        price=item['price'],quantity=item['quantity'])
             # clear the cart
             cart.clear()
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            #----------send mail---------------------------------------------
+            send_mail(
+                "ERB8 Shop",
+                f"Your order has been successfully completed. Your order number is {order.id}",
+                "ericw29Tech@gmail.com",
+                [checkoutEmail],
+                fail_silently=False,
+            )
+            #---------end Send mail------------------------------------------
+            return render(
+                request,
+                "orders/order/created.html",
+                {
+                    "order": order,
+                    "checkoutEmail": checkoutEmail,
+                    "checkoutFirstName": checkoutFirstName,
+                    "checkoutLastName": checkoutLastName,                    
+                },
+            )
     else:
+        print("order create",request.user.id)
+        current_user = User.objects.get(id=request.user.id)
+        # Access the user object
+          
+        print("order create",current_user)
         form = OrderCreateForm()
-    return render(request,
-                  'orders/order/create.html',
-                  {'cart': cart, 'form': form})
+        
+    return render(
+        request,
+        "orders/order/create.html",
+        {"cart": cart, "form": form, "current_user": current_user},
+    )
